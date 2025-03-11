d795fa6176f36aba26eda6959214c5743b0e7aac
Arielle Leon <ariellel@alleninstitute.org>
2025-03-11 13:48:51 -0700

    feat: build initial wrapper for the converter library

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -1,7 +1,54 @@
-""" top level run script """
+"""top level run script"""
+
+import json
+import sys
+from pathlib import Path
+
+from aind_pophys_converter.bergamo_stitcher import (BergamoSettings,
+                                                    BergamoTiffStitcher)
+from aind_pophys_converter.mesoscope_splitter import (TiffSplitterCLI,
+                                                      find_split_directories,
+                                                      from_args)
+
 
 def run():
-    """ basic run function """
-    pass
+    """basic run function"""
+    input_dir = Path("/data")
+    output_dir = Path("/results")
+    session_fp = next(input_dir.rglob("session.json"))
+    data_description_fp = next(input_dir.rglob("data_description.json"))
+    with open(session_fp) as f:
+        session = json.load(f)
+    with open(data_description_fp) as f:
+        data_description = json.load(f)
+    if "Bergamo" in session.get("rig_id", ""):
+        unique_id = data_description["name"]
+        unique_id = "_".join(str(unique_id).split("_")[-3:])
+        bergamo_settings = BergamoSettings(
+            input_dir=input_dir,
+            output_dir=output_dir,
+            unique_id=unique_id,
+            session_fp=session_fp,
+        )
+        bergamo_stitcher = BergamoTiffStitcher(bergamo_settings)
+        bergamo_stitcher.run_converter()
+    elif "multiplane" in data_description["name"]:
+        sys_args = sys.argv[1:]
+        runner = from_args(sys_args)
+        split_directories = find_split_directories(Path(runner.input_dir))
+        if len(split_directories) == 0:
+            runner = TiffSplitterCLI(runner)
+            runner.run_job()
+        else:
+            output_dir = Path(runner.output_dir)
+            for split_dir in split_directories:
+                new_directory = output_dir / split_dir
+                new_directory.mkdir(parents=True, exist_ok=True)
+                with open(new_directory / f"{split_dir}.txt", "w") as f:
+                    f.write(f"{split_dir}.h5")
+    else:
+        pass
 
-if __name__ == "__main__": run()
+
+if __name__ == "__main__":
+    run()

aabf0d71a77b2498cc54bc461be01ed761b656cb
arielleleon <ariellel@alleninstitute.org>
2025-04-23 17:18:25 +0000

    fix: temp hardcoding of BCI folder plane and index no.

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -34,7 +34,7 @@ def run():
         data_description = json.load(f)
     pophys_dir = next(input_dir.rglob("pophys/"))
     if "Bergamo" in session.get("rig_id", ""):
-        unique_id = "MOS2_3_0" # TODO: read from CCF when available
+        unique_id = "MOs2_3_0" # TODO: read from CCF when available
         bergamo_settings = BergamoSettings(
             input_dir=pophys_dir,
             output_dir=output_dir,

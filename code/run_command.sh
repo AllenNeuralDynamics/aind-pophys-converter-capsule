2285957dee7db138a5dd56041c792716004c3b11
seanmcculloch <sean.mcculloch@alleninstitute.org>
2026-04-10 19:27:24 +0000

    Edited datasets.json, environment.json, run, Dockerfile; and changes in 3 other files

---
+++ code/run_command.sh
@@ -0,0 +1,6 @@
+
+# ./run --input_dir /data/multiplane-ophys_827543_2026-01-16_14-03-23 --output_dir /results --temp_dir /scratch --debug true
+./run --input_dir /data/multiplane-ophys_843001_2026-03-25_12-01-30 --output_dir /results --temp_dir /scratch --debug true
+
+
+# ./run --input_dir /data/multiplane-ophys_843001_2026-03-12_12-24-55 --output_dir /results --temp_dir /scratch --debug true
